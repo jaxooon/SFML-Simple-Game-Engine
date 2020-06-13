c276e8f6e505d4f6be24043de560584a43902c96
Jackson Barr <jbarr@uwaterloo.ca>
2020-06-13 15:23:13 -0400

    Simple visual rendered and moveable around the screen

--- SFML_Tutorial/SFML_Tutorial/main.cpp
+++ SFML_Tutorial/SFML_Tutorial/main.cpp
@@ -6,6 +6,11 @@ int main()
 	
 	sf::RenderWindow window(sf::VideoMode(512, 512), "SFML Tutorial", sf::Style::Close | sf::Style::Resize);
 
+	//create a "player" and set its color to cyan
+	sf::RectangleShape player(sf::Vector2f(100, 100));
+	player.setFillColor(sf::Color::Cyan);
+	player.setOrigin(50.0f, 50.0f);
+
 	//running while the window is open
 	while (window.isOpen()) 
 	{
@@ -34,6 +39,35 @@ int main()
 			}
 			
 		}
+
+		//WASD controls
+	/*	if (sf::keyboard::iskeypressed(sf::keyboard::key::a))
+		{
+			player.move(-0.1f, 0.0f);
+		}
+		if (sf::keyboard::iskeypressed(sf::keyboard::key::d))
+		{
+			player.move(0.1f, 0.0f);
+		}
+		if (sf::keyboard::iskeypressed(sf::keyboard::key::w))
+		{
+			player.move(0.0f, -0.1f);
+		}
+		if (sf::keyboard::iskeypressed(sf::keyboard::key::s))
+		{
+			player.move(0.0f, 0.1f);
+		}*/
+
+		//mouse controls
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+		{
+			sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+			player.setPosition((float)mousePos.x, static_cast<float>(mousePos.y));
+		}
+
+		window.clear();
+		window.draw(player);
+		window.display();
 	}
 	return 0;
 }
